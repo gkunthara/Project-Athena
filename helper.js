c4380d6f1cd60f425b62ea9424fbde48a94eaf2a
devin <roche.devin@ymail.com>
2017-10-06 12:31:28 -0700

    append recruiters emails

---
+++ helper.js
@@ -0,0 +1,44 @@
+module.exports = {
+    concatEmails: function(first, last, pattern, domain){
+        var rEmails = []
+        var i =0;
+        while(first[i] != null){
+            switch(pattern) {
+                case '{f}{last}':
+                    rEmails.push((first[i].charAt(0)+last[i]+'@'+domain).toLowerCase())
+                    i++;
+                    break;
+                case '{first}{last}':
+                    rEmails.push((first[i]+last[i]+'@'+domain).toLowerCase())
+                    i++;
+                    break;
+                case '{first}.{last}':
+                    rEmails.push((first[i]+'.'+last[i]+'@'+domain).toLowerCase())
+                    i++
+                    break;
+                case '{first}{l}':
+                    rEmails.push((first[i]+last[i].charAt(0)+'@'+domain).toLowerCase())
+                    i++
+                    break;
+                case '{last}{first}':
+                    rEmails.push((last[i]+first[i]+'@'+domain).toLowerCase())
+                    i++
+                    break;
+                case '{first}':
+                    rEmails.push((first[i]+'@'+domain).toLowerCase())
+                    i++
+                    break;
+                case '{last}':
+                    rEmails.push((last[i]+'@'+domain).toLowerCase())
+                    i++
+                    break;
+                default:
+                    rEmails.push((first[i]+last[i]+'@'+domain).toLowerCase())
+                    i++
+            }
+        }
+
+        console.log(rEmails)
+        return rEmails
+    }
+}
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const request = require('request')
 const axios = require('axios')
+const url = require('url')
 const bodyParser= require('body-parser')
 const GoogleSearch = require('google-search')
+const helpers = require('./helper.js')
 var app = express()
+
 
 var companyName = "Asana"
 
@@ -12,14 +15,15 @@ const googlecx = '013565610204912550067:frat_jzztpg'
 const googleQ = "site:linkedin.com (inurl:in OR inurl:pub) -inurl:dir -inurl:job -inurl:jobs -inurl:jobs2 -intitle:profiles -inurl:groups 'University Recruiter at ";
 const googleSearch = "https://www.googleapis.com/customsearch/v1?key="+googleKey+"&cx="+googlecx+"&q="
 
+const hunterIo1 = 'https://api.hunter.io/v2/domain-search?domain='
+const hunterIo2 = '.com&api_key=da0fc356f1cda1e9f90a5cab277f0ba8a71c9455'
+
 app.set('view engine', 'ejs')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res){
     var recruiters = []
-    console.log(companyName)
-    console.log(req.query.cname)
     if(typeof(req.query.cname) != 'undefined'){
         companyName = req.query.cname
     }
@@ -38,12 +42,13 @@ app.get('/', function(req, res){
                 lRec.push((recruiters[x].split('|')[0]).split(' ')[1])
             }
             var email;
-            axios.get('https://api.hunter.io/v2/domain-search?domain=asana.com&api_key=da0fc356f1cda1e9f90a5cab277f0ba8a71c9455')
+            axios.get(hunterIo1 + companyName + hunterIo2)
                 .then(function(response){
                 email = response.data.data.pattern
                 domain = response.data.data.domain
-    
-                res.render('index', {first: fRec, last: lRec, email: email, domain: domain})
+                rEmails = helpers.concatEmails(fRec, lRec, email, domain)
+
+                res.render('index', {first: fRec, last: lRec, email: email, domain: domain, rEmails:rEmails})
             })
         })
 })
