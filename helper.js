f123b84e3702b4ffc874af2f1df48ce955fabe60
devin <roche.devin@ymail.com>
2017-11-20 19:31:20 -0800

    style and refactor attempt

--- helper.js
+++ helper.js
@@ -2,10 +2,10 @@ const axios = require('axios')
 require('dotenv').config()
 
 const googleQ = "site:linkedin.com (inurl:in OR inurl:pub) -inurl:dir -inurl:job -inurl:jobs -inurl:jobs2 -intitle:profiles -inurl:groups 'University Recruiter at ";
-const googleSearch = "https://www.googleapis.com/customsearch/v1?key="+process.env.googlekey+"&cx="+process.env.googlecx+"&q="
+const googleSearch = "https://www.googleapis.com/customsearch/v1?key="+ process.env.GK +"&cx="+ process.env.GCX +"&q="
 
 const hunterIo1 = 'https://api.hunter.io/v2/domain-search?domain='
-const hunterIo2 = '.com&api_key='+process.env.hunterkey
+const hunterIo2 = '.com&api_key='+process.env.HK
 
 module.exports = {
     concatEmails: function(first, last, pattern, domain){
@@ -48,46 +48,5 @@ module.exports = {
         }
 
         return rEmails
-    },
-
-    googleReq: async (companyName)=>{
-        let recruiters = []
-        axios.get(googleSearch + googleQ + companyName)
-            .then((response) => {
-                var i=0;
-                Object.keys(response.data.items).forEach(function(key){
-                    recruiters[i] = response.data.items[key].title
-                    i++;
-                })
-                var fRec = []
-                var lRec = []
-                for(var i=0; i<recruiters.length; i++){
-                    fRec.push((recruiters[i].split('|')[0]).split(' ')[0])
-                    lRec.push((recruiters[i].split('|')[0]).split(' ')[1])
-                }
-
-                // let hunterio = getEmail(comapnyName)
-                console.log(fRec, lRec)
-                return {
-                    first: fRec,
-                    last: lRec
-                }
-
-            })
-    },
-
-    hunterIo: async (companyName)=>{
-        axios.get(hunterIo1 + companyName + hunterIo2)
-            .then(function(response){
-                var email = response.data.data.pattern
-                domain = response.data.data.domain
-                rEmails = helpers.concatEmails(fRec, lRec, email, domain)
-                console.log(email, domain, rEmails)
-                return {
-                    email: email,
-                    domain: domain,
-                    rEmails: rEmails
-                }
-        })
     }
 }
--- index.js
+++ index.js
@@ -2,69 +2,62 @@ const express = require('express');
 const axios = require('axios')
 const url = require('url')
 const bodyParser= require('body-parser')
-const GoogleSearch = require('google-search')
 const helpers = require('./helper.js')
+require('dotenv').config()
 var app = express()
 
 
 var companyName = "Asana"
 
-// const googleQ = "site:linkedin.com (inurl:in OR inurl:pub) -inurl:dir -inurl:job -inurl:jobs -inurl:jobs2 -intitle:profiles -inurl:groups 'University Recruiter at ";
-// const googleSearch = "https://www.googleapis.com/customsearch/v1?key="+googleKey+"&cx="+googlecx+"&q="
+const googleQ = "site:linkedin.com (inurl:in OR inurl:pub) -inurl:dir -inurl:job -inurl:jobs -inurl:jobs2 -intitle:profiles -inurl:groups 'University Recruiter at ";
+const googleSearch = "https://www.googleapis.com/customsearch/v1?key="+process.env.GK+"&cx="+process.env.GCX+"&q="
 
-// const hunterIo1 = 'https://api.hunter.io/v2/domain-search?domain='
-// const hunterIo2 = '.com&api_key='+hunterApi
+const hunterIo1 = 'https://api.hunter.io/v2/domain-search?domain='
+const hunterIo2 = '.com&api_key='+process.env.HK
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', async (req, res)=>{
+app.get('/', (req, res)=>{
     if(typeof(req.query.cname) != 'undefined'){
         companyName = req.query.cname
-    }else{
-        companyName = 'Asana'
     }
 
-    const foo = await helpers.googleReq(companyName)
-    const bar = await helpers.hunterIo(companyName)
-        
+    axios.get(googleSearch + googleQ + companyName)
+        .then(function(response){
 
-    // console.log(foo)
-    // console.log(bar)
+            let recruiters = []
+            let i=0;
+            let fRec = []
+            let lRec = []
+            
+            Object.keys(response.data.items).forEach(function(key){
+                recruiters[i] = response.data.items[key].title
+                i++;
+            })
 
-    return res.render('index', {
-        first: foo.first, 
-        last: foo.last, 
-        email: bar.email, 
-        domain: bar.domain, 
-        rEmails: bar.rEmails
+            for(i=0; i<recruiters.length; i++){
+                fRec.push((recruiters[i].split('|')[0]).split(' ')[0])
+                lRec.push((recruiters[i].split('|')[0]).split(' ')[1])
+            }
+
+        axios.get(hunterIo1 + companyName + hunterIo2)
+            .then(function(response){
+                let email = response.data.data.pattern
+                domain = response.data.data.domain
+                rEmails = helpers.concatEmails(fRec, lRec, email, domain)
+
+                res.render('index', {
+                    first: fRec, 
+                    last: lRec, 
+                    email: email, 
+                    domain: domain, 
+                    rEmails:rEmails
+                })
+        })
     })
-
-    // axios.get(googleSearch + googleQ + companyName)
-    //     .then(function(response){
-    //         var i=0;
-    //         Object.keys(response.data.items).forEach(function(key){
-    //             recruiters[i] = response.data.items[key].title
-    //             i++;
-    //         })
-    //         var fRec = []
-    //         var lRec = []
-    //         for(var i=0; i<recruiters.length; i++){
-    //             fRec.push((recruiters[i].split('|')[0]).split(' ')[0])
-    //             lRec.push((recruiters[i].split('|')[0]).split(' ')[1])
-    //         }
-
-    //         axios.get(hunterIo1 + companyName + hunterIo2)
-    //             .then(function(response){
-    //             var email = response.data.data.pattern
-    //             domain = response.data.data.domain
-    //             rEmails = helpers.concatEmails(fRec, lRec, email, domain)
-
-    //             res.render('index', {first: fRec, last: lRec, email: email, domain: domain, rEmails:rEmails})
-    //         }).catch(()=> res.render('404'))
-    //     }).catch(()=> res.render('404'))
 })
 
-app.listen(6969)
-console.log('port 6969')
+app.listen(3000)
+console.log('port 3000')
